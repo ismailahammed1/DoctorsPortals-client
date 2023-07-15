f59ba0bcd30a5c5f3d56c57eccfdde3b418cf423
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2023-07-15 21:20:14 +0600

    dhashboardt

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VIT_API_apiKey,
-  authDomain: import.meta.env.VIT_API_authDomain,
-  projectId: import.meta.env.VIT_API_projectId,
-  storageBucket: import.meta.env.VIT_API_storageBucket,
-  messagingSenderId: import.meta.env.VIT_API_messagingSenderId,
-  appId: import.meta.env.VIT_API_appId,
+  apiKey: import.meta.env.VITE_API_apiKey,
+  authDomain: import.meta.env.VITE_API_authDomain,
+  projectId: import.meta.env.VITE_API_projectId,
+  storageBucket: import.meta.env.VITE_API_storageBucket,
+  messagingSenderId: import.meta.env.VITE_API_messagingSenderId,
+  appId: import.meta.env.VITE_API_appId,
 };
 
 // Initialize Firebase
